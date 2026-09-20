c0902a60b0a3e283108cbe43f485223c27126ed4
Edoardo <edoardo@Edoardos-MacBook-Pro.local>
2026-09-20 17:27:32 +0200

    Prepared for deployment to Render

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const viewRouter = require('./routes/viewRoutes');
 const StartDate = require('./models/startDateModel');
 
 const app = express();
-app.set('trust proxy', 1);
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
